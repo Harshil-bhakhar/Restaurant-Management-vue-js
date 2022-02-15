e55677ac1a1d11a33a643a2c2bfdaa63ae730dc7
harshilbhakhar <harshil.b@hashtechy.com>
2022-02-15 16:56:50 +0530

    add table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,39 +1,62 @@
 <template>
-<Header/>
+  <Header />
   <div>
-      <h1>Welcomes <span > { હોમ }</span> PAGE {{name}} </h1>
+    <h1>Welcomes <span> { હોમ }</span> PAGE {{ name }}</h1>
+
+<table border="1" class="basic-table table-headers table table-hover">
+  <thead>
+   <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>Address</th>
+        <th>Contact</th>
+      </tr>
+    <tr style="border: none; box-shadow: none;"></tr>
+  </thead>
+  <tbody>
+     <tr v-for ="item in restaurants" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.Address}}</td>
+        <td>{{item.contect}}</td>
+      </tr>
+  </tbody>
+</table>
   </div>
 </template>
 
 <script>
-import Header from './Header.vue'
+import axios from "axios";
+import Header from "./Header.vue";
 export default {
-name:'Home',
-data(){//name return'
-  return {
-    name:''
-  }
-},
-components:{
-Header
-},
-//page Redirect
-mounted(){ 
-    let user = localStorage.getItem('user-info');
-    this.name= JSON.parse(user).name//name return
-    if(!user)
-    {
-      this.$router.push({name:'SignUp'})
+  name: "Home",
+  data() {
+    //name return'
+    return {
+      name: "",
+      restaurants: [],
+    };
+  },
+  components: {
+    Header,
+  },
+  //page Redirect
+  async mounted() {
+    let user = localStorage.getItem("user-info");
+    // this.name = JSON.parse(user).name; //name return
+    if (!user) {
+      this.$router.push({ name: "SignUp" })
     }
-  }
-}
+    let result = await axios.get("http://localhost:3000/restaurants");
+     console.warn(result);
+     this.restaurants=result.data
+  },
+};
 </script>
 
 <style>
-span
-{
+span {
   color: rgb(212, 207, 216);
-  
 }
- 
+
 </style>
